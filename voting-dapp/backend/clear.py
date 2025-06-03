eef535648110b5691b4fa254739502efd7dfd5d8
AricDesai <aricdesai@gmail.com>
2025-06-03 21:40:17 +0530

    Delete voting-dapp directory

--- voting-dapp/backend/clear.py
+++
@@ -1,7 +0,0 @@
-from pyteal import *
-
-def clear():
-    return Return(Int(1))
-
-if __name__ == "__main__":
-    print(compileTeal(clear(), mode=Mode.Application))
--- voting-dapp/frontend/src/algosdkConfig.js
+++
@@ -1,9 +0,0 @@
-import algosdk from 'algosdk'
-
-const algodToken = 'youralgodtoken'
-const algodServer = 'http://localhost'
-const algodPort = 4001
-
-const algodClient = new algosdk.Algodv2(algodToken, algodServer, algodPort)
-
-export default algodClient
